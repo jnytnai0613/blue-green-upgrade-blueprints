4f6d351cf1ebb120c0426fb8c44ce0784e0f9ff8
jnytnai0613 <surf.0530.junya@gmail.com>
2025-07-04 18:46:26 +0900

    add-codebuild

---
+++ system/common/codebuild/README.md
@@ -0,0 +1,42 @@
+<!-- BEGIN_TF_DOCS -->
+## Requirements
+
+| Name | Version |
+|------|---------|
+| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >=1.12.2 |
+| <a name="requirement_aws"></a> [aws](#requirement\_aws) | >= 5.95.0 |
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| <a name="provider_aws"></a> [aws](#provider\_aws) | 6.2.0 |
+
+## Modules
+
+No modules.
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [aws_codebuild_project.codebuild](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/codebuild_project) | resource |
+| [aws_codebuild_webhook.github](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/codebuild_webhook) | resource |
+| [aws_codeconnections_connection.github](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/codeconnections_connection) | resource |
+| [aws_iam_role.codebuild](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role) | resource |
+| [aws_iam_role_policy.codebuild](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy) | resource |
+| [aws_s3_bucket.codebuild](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket) | resource |
+| [aws_ecr_repository.ecr_repository](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/ecr_repository) | data source |
+| [aws_iam_policy_document.assume_role](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document) | data source |
+| [aws_iam_policy_document.codebuild](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document) | data source |
+
+## Inputs
+
+No inputs.
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| <a name="output_codebuildn_arn"></a> [codebuildn\_arn](#output\_codebuildn\_arn) | ARN of the CodeBuild |
+<!-- END_TF_DOCS -->
--- system/common/codebuild/main.tf
+++ system/common/codebuild/main.tf
@@ -1,5 +1,5 @@
 locals {
-  s3_name                = "codebuild-cache"
+  s3_name                = "codebuild-cache-taniai"
   ecr_name               = "blue-green-ecr"
   codebuild_project_name = "codebuild-project-practice"
   connection_name        = "github-connection"
@@ -9,13 +9,8 @@ resource "aws_s3_bucket" "codebuild" {
   bucket = local.s3_name
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.codebuild.id
-  acl    = "private"
-}
-
 data "aws_ecr_repository" "ecr_repository" {
-  name = locals.ecr_name
+  name = local.ecr_name
 }
 
 data "aws_iam_policy_document" "assume_role" {
@@ -101,22 +96,22 @@ resource "aws_codebuild_project" "codebuild" {
   environment {
     # https://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref-compute-types.html#environment.types
     compute_type = "BUILD_GENERAL1_SMALL"
-    type         = "ARM_CONTAINER"
+    type         = "LINUX_CONTAINER"
 
     # https://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref-available.html
     # EC2インスタンスは"aws/codebuild/ami/amazonlinux-x86_64-base:latest"や
     # "aws/codebuild/ami/amazonlinux-arm-base:latest"のみ。基本的には自前でビルドが必要。
-    image                       = "aws/codebuild/standard:7.0" # Ubuntu 22.04
+    image                       = "aws/codebuild/standard:6.0" # Ubuntu 22.04
     image_pull_credentials_type = "CODEBUILD"
   }
 
   source {
     type                = "GITHUB"
-    location            = "https://github.com/jnytnai0613/terraform_for_aws_practice"
+    location            = "https://github.com/jnytnai0613/blue-green-upgarade-blueprints"
     report_build_status = true
-    #buildspec = "{ビルドスペックを配置のパス}
+    buildspec           = "system/assets/sample-app/container/buildspec.yml"
     auth {
-      type     = "CODECONNECTION"
+      type     = "CODECONNECTIONS"
       resource = aws_codeconnections_connection.github.arn
     }
   }
@@ -129,6 +124,9 @@ resource "aws_codebuild_project" "codebuild" {
   }
 }
 
+# GitHubのsetting/webhookに反映される
+# 初回apply時はこのresourceはコメントアウトしておく
+# CodeConnectionを承認しないと、ARNの無効エラーが出る
 resource "aws_codebuild_webhook" "github" {
   project_name = aws_codebuild_project.codebuild.name
   build_type   = "BUILD"
@@ -140,7 +138,7 @@ resource "aws_codebuild_webhook" "github" {
 
     filter {
       type    = "FILE_PATH"
-      pattern = "system/assets/sample-app/container/*"
+      pattern = "system/assets/sample-app/container/.*"
     }
   }
 }
