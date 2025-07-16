cb41a506a3a4851b0c3311f6fdb9fb39483037ee
jnytnai0613 <surf.0530.junya@gmail.com>
2025-07-16 18:50:31 +0900

    feat: 🎸 VPC Endpoint追加（コメントアウトしている）

--- system/common/network/main.tf
+++ system/common/network/main.tf
@@ -15,25 +15,36 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 6.0.1"
 
-  name = local.vpc_name
-
-  cidr = "10.0.0.0/16"
-
-  azs             = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
-  public_subnets  = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.100.0/24", "10.0.101.0/24", "10.0.102.0/24"]
-
+  name               = local.vpc_name
+  cidr               = "10.0.0.0/16"
+  azs                = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
+  public_subnets     = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets    = ["10.0.100.0/24", "10.0.101.0/24", "10.0.102.0/24"]
   enable_nat_gateway = true
   single_nat_gateway = true
 
   public_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
+    "kubernetes.io/role/elb" = 1
   }
 
   private_subnet_tags = {
     "kubernetes.io/role/internal-elb" = 1
   }
 }
+
+/*
+# コストの面からAWS内外の通信はNAT Gateway経由とする
+# AWS内のEKS to DynamoDB間の通信はVPC Endpoint経由でも良い
+# ただ、以下の事例にあるようにAWS内通信はNAT Gatetewayでもいい
+# https://zenn.dev/simpleform_blog/articles/f048edb9efd2b2
+resource "aws_vpc_endpoint" "dynamodb" {
+  vpc_id            = module.vpc.vpc_id
+  service_name      = "com.amazonaws.ap-northeast-1.dynamodb"
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids = module.vpc.private_route_table_ids
+}
+*/
 
 data "aws_route53_zone" "root" {
   name = local.hosted_zone_name
